ff841ac0ea54340cdc8b861c3675195b57168845
Arkstya <123225658+Arkstya@users.noreply.github.com>
2024-01-20 12:26:38 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,13 @@
+print ("The Hangman Boss has captured 3 of your friends. You must save them all before the Hangman Boss can get them. ")
+print("It would help if you guessed the correct word hurry up because your friends are waiting for you.")
+
+Chosen_Friend_saved = input("The 3 friends' names are Jarad, Jamal, and Jamerion. Who would you like to save first?")
+if Chosen_Friend_saved == "Jarad":
+  print("Jarad is a good choice. He is a very smart and intelligent boy. He is also a very good friend. You have saved Jarad. He is very interested in astronomy. ") 
+
+elif Chosen_Friend_saved =="Jamal":
+  print("Jamal is a good choice, I mean, perhaps he is smart. He is also a very good friend. You have saved Jamal. He is very interested in sports.")
+
+elif Chosen_Friend_saved =="Jamerion":
+  print("Jamerion is a good guy, He is a victim of being bullied by his friends. He is also a very good friend. You have saved Jamerion. ")
+
