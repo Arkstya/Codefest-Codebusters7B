1f9891dfa9facaf0f510dbde5b67313263b4b28f
Arkstya <123225658+Arkstya@users.noreply.github.com>
2024-01-20 11:21:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print ("The Hangman Boss has captured 3 of your friends. You must save them all before the Hangman Boss can get them. It would help if you guessed the correct word hurry up because your friends are waiting for you.")
+
+
+Chosen_Friend_saved = input("The 3 friends names are Jarad, Jamal, Jamerion, Who would you like to save first?")
+if Chosen_Friend_saved == "Jarad":
+
+print ("press space to continue")input()Chosen_Friend_saved = input("The 3 friends' names are Jarad, Jamal, Jamerion. Who would you like to save first?")
+if Chosen_Friend_saved == "Jarad":
+    print("press space to continue") input()
+
+   import matplotlib.pyplot as plt
+    import numpy as np
+
+    circle = plt.Circle((0.5, 0.5), 0.4, color='r', fill=False)
+    fig, ax = plt.subplots()
+    ax.add_artist(circle)
+    plt.axis('equal')
+    plt.show()
